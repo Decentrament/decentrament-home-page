0850446960dbb564191acc6334a0a95d3b396cc3
tommaay <tommy@tomster.dev>
2022-03-13 16:18:18 -0700

    Adding navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,19 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <div className="content-container flex justify-between">
-      <h3 className="text-brand-2">D</h3>
+      <Link href="/">
+        <a className="text-brand-2 text-2xl font-bold">D</a>
+      </Link>
+
       <div className="flex items-center">
-        <Link href="#">
-          <a className="navlink">Home</a>
+        <Link href="/">
+          <a className="navlink transition-effect">Home</a>
         </Link>
         <Link href="#">
-          <a className="navlink ml-10 cursor-default">Docs</a>
+          <a className="navlink transition-effect ml-10 cursor-default">Docs</a>
         </Link>
         <Link href="#">
-          <a className="nav-button ml-10">App</a>
+          <a className="nav-button transition-effect ml-10">App</a>
         </Link>
       </div>
     </div>
